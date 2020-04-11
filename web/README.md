90e8d060aaae63d5d3349a26fb38cec8df53a932
Kaveh Raji <kr@sleekcommerce.com>
2020-04-12 00:15:24 +0200

    first commit

---
+++ web/README.md
@@ -0,0 +1,19 @@
+# README #
+
+### What is this repository for? ###
+
+* Strong framework for building e-commerce solutions with sleekshop based on:
+- bootstrap
+- slim-framework
+- jquery
+- twig as template - engine
+- based on PHP
+* Version 1.0.0
+
+### How do I get set up? ###
+
+* Change some configuration - files
+* Set up the needed classes in the backend
+* All described in the file /setup/readme.txt
+
+### Contribution guidelines ###
---
+++ web/vendor/sleekcommerce/conf.inc.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Conf - file
+ *
+ * @ Kaveh Raji <kr@sleekcommerce.com>
+ */
+define("MYSQL_HOST","localhost");
+define("MYSQL_USER","");
+define("MYSQL_PASSWD","");
+define("MYSQL_DB","");
+//The unique shop - token
+define("TOKEN",getenv("TOKEN"));
+//the default  language
+define("DEFAULT_LANGUAGE","de_DE");
+//Thum-pic size
+define("PRODUCT_IMAGE_THUMB_HEIGHT",30);
+//The Start-Category
+define("START_ID",1);
+//The categories id
+define("CATEGORIES_ID",2);
+//Product-count per page
+define("CATEGORY_PRODUCT_COUNT",20);
+//The sender and receiver of the orders
+define("ORDER_SENDER","");
+?>
